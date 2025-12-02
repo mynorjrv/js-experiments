14b05f2676c4211c6a1cc2fb7ab43469bfce0498
mynorjrv <mynorjrv@gmail.com>
2025-12-02 14:25:50 -0600

    More exercises

--- eloquent_js/4.1-exercises.js
+++ eloquent_js/4.1-exercises.js
@@ -61,12 +61,102 @@ function reverseArrayByIndex(my_array) {
 function reverseArrayInPlace(my_array) {
     for (let i=0; i<=my_array.length/2 - 1; i++) {
         let first = my_array[i];
-        let last = my_array[my_array.length];
+        let last = my_array[my_array.length-1-i];
         my_array[i] = last;
-        my_array[my_array.length] = first;
+        my_array[my_array.length-1-i] = first;
     }
 }
 
 console.log(reverseArray(["A", "B", "C"]))
 console.log(reverseArrayByIndex(["A", "B", "C"]))
-console.log(reverseArrayInPlace(["A", "B", "C"]))
+let arrayValue = [1, 2, 3, 4, 5];
+reverseArrayInPlace(arrayValue);
+console.log(arrayValue);
+
+
+function arrayToList(array) {
+    let rest = null;
+    for (let element of array.reverse()) {
+        rest = {
+            value: element,
+            rest: rest
+        };
+    }
+    return rest;
+}
+
+console.log(arrayToList([1]));
+
+
+function listToArray(list) {
+    let array = [];
+    let rest = list; // hmmm... not sure
+    while (rest['rest'] != null) {
+        array.push(rest['value']);
+        rest = rest['rest'];
+    }
+    array.push(rest['value']);
+    return array;
+}
+
+
+function listToArray_fromHint(list) {
+    let array = [];
+    for (let node = list; node; node = node.rest) {
+        array.push(rest['value']);
+    }
+    return array;
+}
+
+console.log(
+    listToArray(arrayToList([1]))
+);
+
+
+function prepend(element, list) {
+    return {
+        value: element,
+        rest: list
+    };
+}
+
+
+function nth(list, index) {
+    let rest = list; // hmmm... not sure
+    for (let i=0; i<=index; i++) {
+        if (rest==null) return undefined;
+        if (i==index) return rest['value'];
+        rest = rest['rest'];
+    }
+    return undefined;
+}
+
+
+function recursive_nth(list, index) {
+    let rest = list; // hmmm... not sure
+    if (rest==null) return undefined;
+    if (index>0) {
+        index--;
+        return recursive_nth(rest['rest'], index);
+    }
+    if (index==0) return rest['value'];
+    return undefined;
+}
+
+
+console.log(arrayToList([10, 20]));
+console.log(listToArray(arrayToList([10, 20, 30])));
+console.log(prepend(10, prepend(20, null)));
+console.log(nth(arrayToList([10, 20, 30]), 1));
+console.log(nth(arrayToList([10, 20, 30]), 0));
+console.log(nth(arrayToList([10, 20, 30]), 2));
+console.log(nth(arrayToList([10, 20, 30]), 3));
+console.log(nth(arrayToList([10, 20, 30]), -1));
+
+console.log(recursive_nth(arrayToList([10, 20, 30]), 1));
+console.log(recursive_nth(arrayToList([10, 20, 30]), 0));
+console.log(recursive_nth(arrayToList([10, 20, 30]), 2));
+console.log(recursive_nth(arrayToList([10, 20, 30]), 3));
+console.log(recursive_nth(arrayToList([10, 20, 30]), -1));
+
+
