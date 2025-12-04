24d129addde0bf8521222adc444b6d9c4d0e6ac8
mynorjrv <mynorjrv@gmail.com>
2025-12-03 18:31:31 -0600

    Higher order functions I, learning some cool stuff xd

---
+++ eloquent_js/5-higher_order_functions.js
@@ -0,0 +1,239 @@
+// Abstractions everywhere
+
+// Functions are a good way to build abstractions
+
+// Loos abstracts repetition
+// The funny part is repeating abstractions
+
+function repeat(n, action) {
+  for (let i = 0; i < n; i++) {
+    action(i);
+  }
+}
+
+repeat(3, console.log);
+// → 0
+// → 1
+// → 2
+
+// This is commonly seen with lambdas
+let labels = [];
+repeat(5, i => {
+  labels.push(`Unit ${i + 1}`);
+});
+console.log(labels);
+// → ["Unit 1", "Unit 2", "Unit 3", "Unit 4", "Unit 5"]
+
+
+// Functions that get functions as arguments
+// or that returns functions are called higher-order functions
+
+// We can have functions that create functions
+// (currying? not exactly but ajá)
+function greaterThan(n) {
+  return m => m > n;
+}
+let greaterThan10 = greaterThan(10);
+console.log(greaterThan10(11));
+// → true
+
+
+// Fucnitons that change other functions
+// like wtf
+// Is kind of wrapping, reminds me of 
+// decorators without altering the og name
+function noisy(f) {
+  return (...args) => {
+    console.log("calling with", args);
+    let result = f(...args);
+    console.log("called with", args, ", returned", result);
+    return result;
+  };
+}
+noisy(Math.min)(3, 2, 1);
+// → calling with [3, 2, 1]
+// → called with [3, 2, 1] , returned 1
+
+// Functions that define other types
+// of control flow
+function unless(test, then) {
+  if (!test) then();
+}
+
+repeat(3, n => {
+  unless(n % 2 == 1, () => {
+    console.log(n, "is even");
+  });
+});
+// → 0 is even
+// → 2 is even
+
+
+// And there are some higher order functions
+// in the standard language
+["A", "B"].forEach(l => console.log(l));
+// → A
+// → B
+
+
+// Aparently higher order functions are important
+// in data processing? lets see
+let SCRIPTS = {
+  name: "Coptic",
+  ranges: [[994, 1008], [11392, 11508], [11513, 11520]],
+  direction: "ltr",
+  year: -200,
+  living: false,
+  link: "https://en.wikipedia.org/wiki/Coptic_alphabet"
+}
+
+// We can filter the dataset by applying a lambda
+function filter(array, test) {
+  let passed = [];
+  for (let element of array) {
+    if (test(element)) {
+      passed.push(element);
+    }
+  }
+  return passed;
+}
+
+console.log(filter(SCRIPTS, script => script.living));
+// → [{name: "Adlam", …}, …]
+
+
+// filter is actually in the standard language
+console.log(SCRIPTS.filter(s => s.direction == "ttb"));
+// → [{name: "Mongolian", …}, …]
+
+
+// We have also a map
+function map(array, transform) {
+  let mapped = [];
+  for (let element of array) {
+    mapped.push(transform(element));
+  }
+  return mapped;
+}
+
+let rtlScripts = SCRIPTS.filter(s => s.direction == "rtl");
+console.log(map(rtlScripts, s => s.name));
+// → ["Adlam", "Arabic", "Imperial Aramaic", …]
+// Which is also in the standard language
+
+
+// We have a reduce (also called fold)
+// That summarizes something in the array with 
+// a single value
+function reduce(array, combine, start) {
+  let current = start;
+  for (let element of array) {
+    current = combine(current, element);
+  }
+  return current;
+}
+
+console.log(reduce([1, 2, 3, 4], (a, b) => a + b, 0));
+// → 10
+// This sums the values, but the function that
+// is used to reduce is given by a lambda
+
+// It is also in the standard language and if we
+// do not pass the start argument, the functions 
+// uses the first element of the array
+console.log([1, 2, 3, 4].reduce((a, b) => a + b));
+// → 10
+
+// We can reduce multiple times to find some complex stuff
+// And felt confusing
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
+
+// The syntaxis is something like:
+// reduce(callbackFn, initialValue)
+// and callbackFn may have the following arguments:
+// callbackFn(accumulator, currentValue, currentIndex, array)
+
+
+let array_of_arrays = [
+    [1, 2, 3, 4], 
+    [5, 6, 7, 8]
+]
+
+// sum of minimums
+array_of_arrays.reduce(
+    (accumulator, currentValue) => accumulator+Math.min(...currentValue),
+    0
+)
+
+// In the context of the dataset
+function characterCount(script) {
+  return script.ranges.reduce((count, [from, to]) => {
+    return count + (to - from);
+  }, 0);
+}
+
+console.log(SCRIPTS.reduce((a, b) => {
+  return characterCount(a) < characterCount(b) ? b : a;
+}));
+// → {name: "Han", …}
+
+// The same functionality but without using 
+// reduce may be obtained by 
+let biggest = null;
+for (let script of SCRIPTS) {
+  if (biggest == null ||
+      characterCount(biggest) < characterCount(script)) {
+    biggest = script;
+  }
+}
+console.log(biggest);
+// → {name: "Han", …}
+
+// Which does not look that much worse
+
+
+// aparently the catch is Composability
+function average(array) {
+  return array.reduce((a, b) => a + b) / array.length;
+}
+
+console.log(Math.round(average(
+  SCRIPTS.filter(s => s.living).map(s => s.year))));
+// → 1165
+console.log(Math.round(average(
+  SCRIPTS.filter(s => !s.living).map(s => s.year))));
+// → 204
+
+// We can make easy to read pipelines by
+// applying one function at a time.
+
+// The trade off is that calling those
+// functions takes more resources than
+// making a long complex loop
+let total = 0, count = 0;
+for (let script of SCRIPTS) {
+  if (script.living) {
+    total += script.year;
+    count += 1;
+  }
+}
+console.log(Math.round(total / count));
+// → 1165
+
+
+// We have a some function 
+// And this one seems strange
+// 
+function characterScript(code) {
+  for (let script of SCRIPTS) {
+    if (script.ranges.some(([from, to]) => {
+      return code >= from && code < to;
+    })) {
+      return script;
+    }
+  }
+  return null;
+}
+
+console.log(characterScript(121));
+// → {name: "Latin", …}
