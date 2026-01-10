aa3207d96e77e1f6e40d346bd480361b8e12c2a8
mynorjrv <mynorjrv@gmail.com>
2026-01-09 23:57:50 -0600

    Fist aproach to asynchonicity

---
+++ eloquent_js/11-async.js
@@ -0,0 +1,9 @@
+// Who can wait quietly while the mud settles?
+// Who can remain still until the moment of action?
+
+// - Laozi, Tao Te Ching
+
+
+// Asynchronous is not parallel, is waiting
+// This waiting becomes explicit when
+// using an asynchronous model
