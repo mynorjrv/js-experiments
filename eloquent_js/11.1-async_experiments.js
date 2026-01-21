9278a1f2765a191a7e5a96f1b2acfc014e5e4bd1
mynorjrv <mynorjrv@gmail.com>
2026-01-21 12:04:21 -0600

    About asyc failure handling

--- eloquent_js/11.1-async_experiments.js
+++ eloquent_js/11.1-async_experiments.js
@@ -59,3 +59,60 @@ random_modified.then(value=>console.log(value))
 // It is actually stranger than I expected, but the idea of 
 // using experiments instead of following the book feels like
 // the best decission ever.
+
+
+// I do not know if this part should be in the original
+// notes but im just going to continue here
+
+// Async also needs a way to fail
+// With callbacks, ensuring that the failures are difficult
+// to be properly reorted to the callbacks. 
+
+// The common convention is to use two values:
+// someAsyncFunction((error, value) => {
+//   if (error) handleError(error);
+//   else processValue(value);
+// });
+
+
+// Aparently promises are easier to handle, they can be resolved
+// (the action finished successfully) or rejected (it failed).
+// Resolve handlers (as registered with then) are called only
+// when the action is succesful, and rejections are propagated.
+
+// Rejections also provides a value, usually called the reason.
+// When an exception causes the rejection, the exception value is
+// used as the reason. Promise.reject create a new, immediately
+// rejected promise.
+
+// To handle rejections, promises have a catch method xd
+// It is basically a then but for rejections
+// Buuuuut xd then actually can accept two arguments, an
+// acceptHandler and a rejectHandler like then(acceptHandler, rejectHandler)
+
+// Im not sure if I undestand... A function passed to the Promise
+// constructor receives a second argument, alongside the resolve
+// function, which it can use to reject the new promise.(?)
+
+// I think this chapter may be better if instead of using a 
+// fictional readTextFile we used vanillajs functions as examples
+// function textFile(filename) {
+//   return new Promise((resolve, reject) => {
+//     readTextFile(filename, (text, error) => {
+//       if (error) reject(error);
+//       else resolve(text);
+//     });
+//   });
+// }
+
+
+// JAJAJAJA WTF, a convoluted pipeline of values and failures
+new Promise((_, reject) => reject(new Error("Fail")))
+  .then(value => console.log("Handler 1:", value))
+  .catch(reason => {
+    console.log("Caught failure " + reason);
+    return "nothing";
+  })
+  .then(value => console.log("Handler 2:", value));
+// → Caught failure Error: Fail
+// → Handler 2: nothing
